a8cf394baa9f8baf9ed16e2c8000e8637abc87dc
Aizel Nunag <izelllim@gmail.com>
2023-04-04 06:46:45 +0800

    added hacker rank practice

--- Nokia_Exams/Program.cs
+++ Nokia_Exams/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -15,7 +16,17 @@ namespace Nokia_Exams
 
         public static void Testrun()
         {
-            OddOrEven2(5);
+
+
+            //OddNumberToarray(2, 5);
+            Console.WriteLine(OddNumberToarray(2, 4));
+            //List<int> numbers = new List<int>() { 1, 2, 5, 7, 8, 10 };
+            //int x = 5;
+            //string v = FindNumber(numbers, x);
+            //Console.WriteLine(v);
+
+
+            //OddOrEven2(2);
             //Console.WriteLine("HELLO");
             //Fabonacci();
             //Multiplicationtable(5);
@@ -35,7 +46,7 @@ namespace Nokia_Exams
 
             for (int i = 1; i <= size; i++)
             {
-                for(int k = 1; k <= size; k++)
+                for (int k = 1; k <= size; k++)
                 {
                     value = i * k;
                     Console.Write("\t" + value);
@@ -61,7 +72,7 @@ namespace Nokia_Exams
 
         public static void Fabonacci()
         {
-            int x=0, y=1;
+            int x = 0, y = 1;
             for (int i = 0; i <= 5; i++)
             {
                 Console.Write(x);
@@ -74,7 +85,7 @@ namespace Nokia_Exams
         }
         public void OddOrEven(int numberToDetermine)
         {
-            if (numberToDetermine %2 == 0)
+            if (numberToDetermine % 2 == 0)
             {
                 Console.WriteLine("EVEN");
             }
@@ -86,21 +97,21 @@ namespace Nokia_Exams
         public static void OddOrEven2(int numberToDetermine)
         {
             string x;
-            x= (numberToDetermine % 2 == 0)? "EVEN" : "ODD";
+            x = (numberToDetermine % 2 == 0) ? "EVEN" : "ODD";
             Console.WriteLine(x);
         }
 
         public static void BubbleSort()   //bubble sort 
         {
-            
-            int [] listNumber = { 5,1,3,13,134,12,13,4,122,145,1156};
+
+            int[] listNumber = { 5, 1, 3, 13, 134, 12, 13, 4, 122, 145, 1156 };
             int holdNumber;
 
             for (int i = 0; i < listNumber.Length; i++)
             {
                 for (int secondHolderNumber = 0; secondHolderNumber < listNumber.Length - 1; secondHolderNumber++)
                 {
-                    if (listNumber[secondHolderNumber] > listNumber[secondHolderNumber +1 ])
+                    if (listNumber[secondHolderNumber] > listNumber[secondHolderNumber + 1])
                     {
                         holdNumber = listNumber[secondHolderNumber + 1];
                         listNumber[secondHolderNumber + 1] = listNumber[secondHolderNumber];
@@ -108,7 +119,7 @@ namespace Nokia_Exams
                     }
                 }
             }
-            for(int i = 0; i < listNumber.Length ; i++)
+            for (int i = 0; i < listNumber.Length; i++)
             {
                 Console.WriteLine(listNumber[i] + ", ");
             }
@@ -117,21 +128,21 @@ namespace Nokia_Exams
         public static void Palindrome() //palindrome
         {
             string word = "HELLO";
-            string rev="";
-            for(int i = word.Length-1 ; i >= 0 ; i--)
+            string rev = "";
+            for (int i = word.Length - 1; i >= 0; i--)
             {
                 rev += word[i];
             }
             Console.WriteLine(rev);
             Console.WriteLine(rev == word ? "PALINDROME" : "NOT PALINDROME");
         }
-       
+
         public static void NumberToMonths(int numberToMonths) //numbertoMonths
         {
 
             String[] Months = { "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December" };
-            
-            string display = numberToMonths < 13 ? Months[numberToMonths-1]:"ERROR" ;
+
+            string display = numberToMonths < 13 ? Months[numberToMonths - 1] : "ERROR";
             Console.Write(display);
 
         }
@@ -153,7 +164,7 @@ namespace Nokia_Exams
             string snum = num.ToString();
             int total = 0;
             int[] listToSumInt = new int[50];
-          
+
             for (int i = 0; i < snum.Length; i++)
             {
                 listToSumInt[i] = Convert.ToInt32(snum[i] - 48);
@@ -183,7 +194,7 @@ namespace Nokia_Exams
             int[] counter = new int[50];
 
             string mostused = "";
-            for(int i =0;i < input.Length; i++)
+            for (int i = 0; i < input.Length; i++)
             {
                 split[i] = input[i];
                 //Console.WriteLine(split[i]);
@@ -199,25 +210,55 @@ namespace Nokia_Exams
                         Console.WriteLine(counter[i]);
                     }
                 }
-               
+
             }
             for (int i = 0; i < input.Length; i++)
             {
-                Console.WriteLine( counter[i]);
+                Console.WriteLine(counter[i]);
             }
             for (int i = 0; i < input.Length; i++)
             {
-                if(counter[i] > counter[i + 1])
+                if (counter[i] > counter[i + 1])
                 {
                     mostused = counter[i].ToString();
                 }
             }
-          
+
             return mostused;
         }
+        public static string FindNumber(List<int> arr, int k) //HACKER RANK
+        {
+            string x = "YES";
+            for (int i = 0; i <= arr.Count - 1; i++)
+            {
+                if (arr[i] == k)
+                {
 
+                    x = "YES";
+                    break;
+                }
+
+                else x = "NO";
+            }
+            return x;
+        }
+
+        public static List<int> OddNumberToarray(int l, int r)  //HACKER RANK   
+        {
+            int x = 0;
+            List<int> numbers = new List<int>() { };
+            for (int i = l; i <= r; i++)
+            {
+                if (i % 2 != 0)
+                {
+                    numbers.Add(i);
+                }
+            }
+            return numbers;
+        }
+    
+
+        
         //outputprime
     }
-
-
 }
